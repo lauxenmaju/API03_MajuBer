92ec91ff1410b5192c9b169025c24f469d7e3485
lauxenmaju <lauxen.maria@escola.pr.gov.br>
2024-08-30 11:09:53 -0300

    first commit

--- api3.js
+++ api3.js
@@ -1,3 +1,6 @@
+//eliminar uma task existente
+//inserir uma nova task
+
 //1 require
 const express = require ('express');
 const mysql = require('mysql2');
@@ -5,19 +8,18 @@ const cors = require('cors');
 
 const mysql_config = require ('./inc/mysql_config');
 const functions = require('./inc/functions');
+const { json } = require('express');
 
 //2 criação de duas constantes para a definição da disponibilidade da api e da versão da api
 const API_AVAILABILITY = true;
-const API_VERSION = "2.0.0";
-
-//3 iniciar o server
+const API_VERSION = "3.0.0";
 
 const app = express()
 app.listen(3000,()=>{
     console.log("APi esta executando")
 })
 
-//4 checar se a API esta disponivel
+
 
 app.use((req,res,next)=>{
     if(API_AVAILABILITY){
@@ -27,27 +29,35 @@ app.use((req,res,next)=>{
     }
 })
 
-//5 mysql connection
+
 
 const connection = mysql.createConnection(mysql_config);
 
-//6 cors
+
 
 app.use(cors());
 
-//7 rotas 
-//rotas em inicial que vai dizer que a APi esta disṕonivel 
+//tratamento dos posts params 
+app.use(json());
+//instrução que pede que o express trate os dados com json
 
+app.use(express.urlencoded({extended: true}));
+
+//quando é enviado um pedido atraves do metodo post, os dados enviados atráves de um formulário podem ser interpretados
+//SEM ESSES DOIS MIDLEWARE NÃO SERIA POSSIVEL BUSCAR OS PARAMETROS
+
+//ROTAS
+//rota de entrada
 app.get('/',(req,res)=>{
     res.json(functions.response('sucesso','api esta rodando',0,null))
 })
 
-//9 rota para pegar todas as tarefas
+//rota para pegar todas as tarefas
 app.get('/tasks', (req,res)=>{
      connection.query("SELECT * FROM tasks",(err,rows))
 })
 
-//10 rota para pegar a task pelo id
+//rota para pegar a task pelo id
 app.get("/tasks/:id",(req,res)=>{
     const id = req.params.id;
     connection.query('SELECT *FROM tasks WERE id=?',(id),(err,rows)=>{
@@ -65,7 +75,7 @@ app.get("/tasks/:id",(req,res)=>{
     })
 })
 
-//11 atualizar o status de uma task  metodo put 
+//rotas para atualizar o status de uma task  metodo put 
 app.put('/tasks/:id status/ status',(req,res)=>{
     const id = req.params.id;
     const status = req.params.status;
@@ -84,7 +94,52 @@ app.put('/tasks/:id status/ status',(req,res)=>{
     })
 })
 
-//8 mydlware para caso alguma rota não seja encontrada 
+//rota para deletar uma tarefa
+app.delete('/tasks/:id/delete', (req,res)=>{
+    const id = req.params.id;
+    connection.query('DELETE FROM tasks WERE id=?',[id],(err,rows)=>{
+        if(!err){
+            if(rows.affectedRows>0){
+                res.json(functions.response('Sucesso','Tasks deletada', rows.affectedRows,null))
+            }
+            else{
+                res.json(functions.response('Atenção','Task não encontrada',0,null))
+            }
+        }
+        else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+})
+
+//rota para inserir uma nova task
+app.put('/tasks/create',(req,res)=>{
+    //midleware para a recepção dos dados da tarefa (task)
+
+    //pegando os dados da request
+    const post_data= req.body;
+
+    //checar se não estamos recebendo json vazia
+    if(post_data ==undefined){
+        res.json(functions.response('Atenção','Sem dados de uma nova task',0,null))
+        return;
+    }
+    const task = post_data.task;
+    const status = post_data.status;
+
+    //Inserindo a nova task 
+    connection.query('INSERT INTO tasks(task,status,created_at,update_at) VALUES(?,?,NOW(),NOW()',[task,status],(err,rows)=>{
+        if(!err){
+            res.json("Sucesso","Task cadastrada no banco",rows.affectedRows,null)
+        }
+        else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+   
+})
+
+//mydlware para caso alguma rota não seja encontrada 
 
 app.use((req,res)=>{
     res.json(functions.response('Atenção','rota não encontrada',0,null))
